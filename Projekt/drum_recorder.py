76f0096e5632b11a749fe00dabe724b5e442979c
m-wrzr <martin.wrzr@gmail.com>
2016-06-27 12:59:51 +0200

    added recorder;

--- Projekt/drum_recorder.py
+++ Projekt/drum_recorder.py
@@ -1,16 +1,21 @@
 import serial
 import socket
+import signal
 
 import subprocess
+
+'''
+record drum sounds for reference values
+'''
 
 
 # mapping capacitive sensor to sound
 def play_sound(touch):
     sound_map = {
-        1: "sounds/CH.mp3",
-        3: "sounds/CL.mp3",
-        5: "sounds/RS.mp3",
-        7: "sounds/SD0000.mp3"
+        1: "sounds_mp3/CH.mp3",
+        3: "sounds_mp3/CL.mp3",
+        5: "sounds_mp3/RS.mp3",
+        7: "sounds_mp3/SD0000.mp3"
     }
     subprocess.Popen(["afplay", sound_map[touch]])
 
@@ -18,6 +23,10 @@ def play_sound(touch):
 # parse RECV b'0\r\n'
 def parse_arduino_str(ard_str):
     return int(str(ard_str).replace("\\n", "").replace("\\r", "").replace("b", "").replace("\'", ""))
+
+
+def tick():
+    pass
 
 
 # Establish the connection on a specific port
@@ -29,6 +38,15 @@ listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
 listen_socket.listen(1)
 print('Serving HTTP on port %s ...')
+
+
+for i in range(100000):
+    try:
+        subprocess.call(['python', 'a.py'], timeout=1)
+    except subprocess.TimeoutExpired:
+        sys.exit(1)
+
+
 
 while True:
     try:
