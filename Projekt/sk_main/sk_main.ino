9ce68224b01d0ead46986bec31c11c5c71756cd1
Julia Kindelsberger <julia.kindelsberger@gmx.at>
2016-07-04 12:21:54 +0200

    added pulse sensor to main

--- Projekt/sk_main/sk_main.ino
+++ Projekt/sk_main/sk_main.ino
@@ -4,6 +4,23 @@
 // #include <Adafruit_TiCoServo.h>
 // #include <known_16bit_timers.h>
 
+/* PulseSensor */
+//  Variables
+int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
+int blinkPin = 9;                // pin to blink led at each beat
+int fadePin = 5;                  // pin to do fancy classy fading blink at each beat
+int fadeRate = 0;                 // used to fade LED on with PWM on fadePin
+
+// Volatile Variables, used in the interrupt service routine!
+volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
+volatile int Signal;                // holds the incoming raw data
+volatile int IBI = 600;             // int that holds the time interval between beats! Must be seeded!
+volatile boolean Pulse = false;     // "True" when User's live heartbeat is detected. "False" when not a "live beat".
+volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
+
+// Regards Serial OutPut  -- Set This Up to your needs
+static boolean serialVisual = true;   // Set to 'false' by Default.  Re-set to 'true' to see Arduino Serial Monitor ASCII Visual Pulse
+
 /* CapSensor */
 Adafruit_MPR121 cap = Adafruit_MPR121();
 uint16_t lasttouched = 0;
@@ -28,7 +45,13 @@ int32_t black = strip.Color(0, 0, 0);
 void setup() {
   while (!Serial);
 
-  Serial.begin(115200);
+  pinMode(blinkPin, OUTPUT);        // pin that will blink to your heartbeat!
+  pinMode(fadePin, OUTPUT);         // pin that will fade to your heartbeat!
+  Serial.begin(115200);             // we agree to talk fast!
+  interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS
+  // IF YOU ARE POWERING The Pulse Sensor AT VOLTAGE LESS THAN THE BOARD VOLTAGE,
+  // UN-COMMENT THE NEXT LINE AND APPLY THAT VOLTAGE TO THE A-REF PIN
+  //   analogReference(EXTERNAL);
   Serial.setTimeout(50);
 
   /* CapSensor Setup */
@@ -46,16 +69,31 @@ void loop() {
 
   if (Serial.available()) {
 
+    // pulse sensor
+    serialOutput() ;
+
+    if (QS == true) {    // A Heartbeat Was Found
+      //Serial.print(BPM);                 // BPM and IBI have been Determined
+      // Quantified Self "QS" true when arduino finds a heartbeat
+      fadeRate = 255;         // Makes the LED Fade Effect Happen
+      // Set 'fadeRate' Variable to 255 to fade LED with pulse
+      serialOutputWhenBeatHappens();   // A Beat Happened, Output that to serial.
+      QS = false;                      // reset the Quantified Self flag for next time
+    }
+
+    ledFadeToBeat();                      // Makes the LED Fade Effect Happen
+    delay(20);                             //  take a break
+    /********/
     // TODO check for motor update
 
     int input = Serial.parseInt();
 
     if (input > 0) {
 
-      /*
-         red = 1, green = 2, yellow = 3, blue = 4
-         intensity from 0 to 3 lights activated
-      */
+
+      //red = 1, green = 2, yellow = 3, blue = 4
+      //intensity from 0 to 3 lights activated
+
       int color = input / 10;
       int intensity = input % 10;
 
@@ -79,7 +117,9 @@ void touchedPin() {
 
   for (uint8_t i = 0; i < 12; i++) {
     if ((cap.filteredData(i) < threshold_on_alu_things) && !(lasttouched & _BV(i))) {
-      Serial.println(i);
+      Serial.println(i );
+      Serial.print( '.' );
+      Serial.println(BPM);
     }
   }
 
@@ -102,4 +142,11 @@ void setPixels(int from, int to, int32_t color) {
   }
   strip.show();
 }
+
+void ledFadeToBeat() {
+  fadeRate -= 15;                         //  set LED fade value
+  fadeRate = constrain(fadeRate, 0, 255); //  keep LED fade value from going into negative numbers!
+  analogWrite(fadePin, fadeRate);         //  fade LED
+}
+
 
