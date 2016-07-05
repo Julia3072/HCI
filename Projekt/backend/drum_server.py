ab3753b59b6b38750f150163e4602fd0303bd87a
m-wrzr <martin.wrzr@gmail.com>
2016-07-05 21:55:31 +0200

    refactored python stuff;

--- Projekt/backend/drum_server.py
+++ Projekt/backend/drum_server.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 
-from json import loads
 from json import load
 from math import ceil
 from multiprocessing import Process, Manager
 
 from serial import Serial, SerialException
 from subprocess import Popen
-from sys import maxsize, exit
+from sys import maxsize
 from time import sleep
 
-from QLearning import QLearning
-from DifficultyCalculator import calculateDifficultyScore
+from .q_learning import QLearning
+from .calculations import calculate_difficulty_score, calculate_song_score
 
-'''
+"""
 game loop for playing
-'''
+"""
 
 sound_map = {
     1: "sounds_mp3/CH.mp3",
@@ -30,17 +29,19 @@ color_map = {
     4: "blue"
 }
 latency_light, latency_correct, latency_double, max_tick = 100, 20, 5, 0.02
-heart_rate_bound_low = 70
-heart_rate_bound_high = 80
 
 
-# writes color changes to arduino
 def process_lighting(color_id: int, intensity: int):
+    """
+    writes color changes to arduino
+    """
     serial.write("{}{}\n".format(color_id, intensity).encode('ascii'))
 
 
-# turns of lights after indication from song_ref
 def process_lights_turn_off(color_id: int, timer: int):
+    """
+    turns of lights after indication from song_ref
+    """
     # TODO adapt color turning off - change only one light at a time, at first red1, red2, red3, then wait and blacken
     # TODO maybe priority queue, where the indication of a next light has priority over the turning off
     sleep(timer)
@@ -48,6 +49,9 @@ def process_lights_turn_off(color_id: int, timer: int):
 
 
 def update_lights(timeslot: int, _sref, _curr_play):
+    """
+    set lights on arduino if in reference json
+    """
     for j in range(1, 5):
 
         # if sound played within next 20 timeslots
@@ -66,8 +70,10 @@ def update_lights(timeslot: int, _sref, _curr_play):
             sleep(max_tick)
 
 
-# function wrapper for process
 def tick(timeslot: int, _sref, _curr_ply, _n_corr, _n_insg, _heart_avg):
+    """
+    function wrapper for process
+    """
     try:
 
         # if arduino needs lighting update only do this to not overwrite serial
@@ -107,36 +113,23 @@ serial = Serial("/dev/cu.usbmodem1411", 115200)
 
 # read song list
 with open("songList.json") as data_file:
-    songListParsed = load(data_file)
-songList = songListParsed["songs"]
+    songList = load(data_file)["songs"]
 
-# init q learning
-initialMatrix = [0.0]*len(songList)
-for i in range(0, len(songList)):
-    initialMatrix[i] = calculateDifficultyScore("sounds_json/{}.json".format(songList[i]))
-
-qLearning = QLearning(initialMatrix)
+    # init q learning with initial matrix
+    qLearning = QLearning(
+        [calculate_difficulty_score("sounds_json/{}.json".format(songList[i])) for i in range(0, len(songList))])
 
 while True:
-    # used for entering a song name
-    #song_name = input("Enter mp3 filename (w/o ending): ")
-    #if song_name == "exit":
-    #    exit(0)
-
     # get next song
-    current_song_index = qLearning.getNext()
-    song_name = songList[current_song_index]
-
-    print(songList)
-    print(qLearning.getQMatrix())
-    print(current_song_index)
+    current_song_index = qLearning.get_next()
+    song_name = songList[qLearning.get_next()]
 
     curr_song = Popen(["afplay", "sounds_mp3/{}.mp3".format(song_name)])
 
     with open("sounds_json/{}.json".format(song_name), "r") as song_ref:
-        song_ref = loads(song_ref.readlines()[0])
-        # init reference dictionary
+        song_ref = load(song_ref)
 
+        # init reference dictionary
         song_ref = Manager().dict({color_map[x]: song_ref[color_map[x]] for x in range(1, 5)})
         sums_ref = sum([len(song_ref[color_map[x]]) for x in range(1, 5)])
 
@@ -161,28 +154,10 @@ while True:
                 p.terminate()
                 p.join()
 
-        n_wrong = n_insg.value - n_corr.value
-        print(n_corr.value)
-        print(n_insg.value)
-        print(sums_ref)
+        score = calculate_song_score(n_corr.value, n_insg.value, sums_ref, heart_avg)
 
-        avg_heart_rate = sum(list(heart_avg)) / len(heart_avg)
-        print(avg_heart_rate)
-
-        # amount correctly played subtracted the number of unnecessary touches
-        score = int(100 * (n_corr.value / sums_ref) - max((100 * (n_insg.value - sums_ref) / sums_ref), 0))
-        # add bonus points based on average heart rate
-        if avg_heart_rate < heart_rate_bound_low:
-            score -= 5
-        if avg_heart_rate > heart_rate_bound_high:
-            score += 5
-
-        # make sure 0 <= score <= 100
-        score = min(max(score, 0), 100)
-        
         # send to Qlearning part
-        qLearning.updateQMatrix(current_song_index, score)
-        print(score)
+        qLearning.update_q_matrix(current_song_index, score)
 
         # TODO send to motor
         # serial.write("{}{}\n".format(9, score / 10).encode('ascii'))
---
+++ Projekt/backend/q_learning.py
@@ -0,0 +1,58 @@
+from random import randint
+
+
+class QLearning:
+    qMatrix = []
+    alpha = 0.5
+    gamma = 0.5
+    targetValue = 75
+    minValue = 0
+    maxValue = 100
+    epsilon = 10
+    phi = 0.2
+
+    def __init__(self, initial_matrix):
+        self.qMatrix = initial_matrix
+
+    def get_q_matrix(self):
+        return self.qMatrix
+
+    def calculate_diff(self):
+        return self.maxValue - self.minValue
+
+    def calculate_reward(self, score):
+        return self.calculate_diff() - abs(score - self.targetValue)
+
+    def update_q_matrix(self, index, score):
+        reward = self.calculate_reward(score)
+
+        value = self.qMatrix[index]
+        max_value = max(self.qMatrix)
+        self.qMatrix[index] = value + self.alpha * (reward + self.gamma * max_value - value)
+
+    def get_next(self):
+        if randint(0, 100) < self.epsilon:
+            return randint(0, len(self.qMatrix) - 1)
+
+        copy = list(reversed(sorted(self.qMatrix)))
+
+        # noinspection PyTypeChecker
+        compare_value = copy[int(self.phi * len(copy))]
+
+        matches = []
+        for i in range(0, len(self.qMatrix)):
+            if self.qMatrix[i] >= compare_value:
+                matches.append(i)
+
+        return matches[randint(0, len(matches) - 1)]
+
+
+"""
+x = QLearning(5)
+x.update_q_matrix(0, 75)
+x.update_q_matrix(1, 90)
+x.update_q_matrix(3, 60)
+print(x.get_q_matrix())
+print("")
+print(x.get_next())
+"""
